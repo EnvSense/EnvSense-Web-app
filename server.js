6b19a8804fa04a5bc3e3663d5bb9f5471e4f5938
youngcoma-server <ykhorizon.light@gmail.com>
2017-09-21 16:36:40 +0800

    backup for css adding

--- server.js
+++ server.js
@@ -3,35 +3,102 @@ var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var moment = require('moment');
+
+/* Socket Configuration */
+updateInterval = 1000;
+CEHCK_TIME_CNT = 10;
 
 
+/* Database Configuration */
+var preFetchRow = {};
 var connection = mysql.createConnection({
-  host     : '140.114.71.114',
-  user     : 'mashroom',
-  password : 'KHf7f2vxJPAUY2jR',
-  database : 'earthworm'
+    host: 'localhost',
+    user: 'mashroom',
+    password: 'KHf7f2vxJPAUY2jR',
+    database: 'earthworm'
 });
-
 connection.connect();
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
 
-  console.log('The solution is: ', rows[0].solution);
+
+// Query Todat Env. Data
+var todayData = null;
+connection.query('SELECT * FROM `env_data` WHERE DATE(`datetime`) = "2016-10-16"', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('row length', rows.length);
+    todayData = rows;
+    // console.log(todayData);
 });
 
-server.listen(8080);
+
+
+// Check if lastest data is equal previous one or not
+
+
+
+// Server Routing Settings
+server.listen(16888);
 app.use('/static', express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/socket.html');
+    res.sendFile(__dirname + '/socket.html');
+    // res.type('application/json');
+    // res.json(todayData);
+});
+app.post('/todaydata', function(req, res) {
+    res.type('application/json');
+    res.json(todayData);
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+/* Socket Handling */
+io.on('connection', function(socket) {
+    console.log('hello socket');
+
+
+    var updateCount = CEHCK_TIME_CNT;
+    setInterval(function() {
+
+        // console.log('socket_id: ', socket.id);
+        var emitData = {};
+
+        // Every 60 sec check database exist a new data or not
+        if (updateCount === 0) {
+
+            // console.log('update event');
+            // console.log(updateCount);
+            updateCount = CEHCK_TIME_CNT;
+
+            // Query Last Row of Env. data
+            connection.query('SELECT * FROM `env_data` ORDER BY `id` DESC LIMIT 1', function(err, rows, fields) {
+                if (err) throw err;
+
+                console.log('in preFetchRow');
+                if (preFetchRow.id !== rows[0].id) {
+                    preFetchRow = rows[0];
+                    emitData.newData = rows[0];
+                    console.log('last data', rows[0]);
+                } else {
+                    console.log('no new data in database');
+                    emitData.newData = null;
+                }
+
+            });
+            emitData.updateCount = updateCount;
+            emitData.lastUpdateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+        // Continously increase updateCount
+        } else {
+            updateCount = updateCount - 1;
+            emitData.updateCount = updateCount;
+            // console.log('updateCount', updateCount);
+        }
+        socket.emit('serverTime', emitData);
+    }, updateInterval);
+
+
 });
+
 
 
 
